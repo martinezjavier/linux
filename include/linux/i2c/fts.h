cafebfad687d29ddc23608d160666efc54334193
Beomho Seo <beomho.seo@samsung.com>
2016-08-17 17:41:26 +0900

    LOCAL / input: touchscreen: Add FTS_TS touchsreen driver
    This patch add STMicroelectronics FTS touchscreen controller
    which supprots the multi-touch protocol.
    
    Signed-off-by: Beomho Seo <beomho.seo@samsung.com>
    Signed-off-by: Andi Shyti <andi.shyti@samsung.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

---
+++ include/linux/i2c/fts.h
@@ -0,0 +1,17 @@
+#ifndef _LINUX_FTS_I2C_H_
+#define _LINUX_FTS_I2C_H_
+
+struct fts_i2c_platform_data {
+	int max_x;
+	int max_y;
+	int max_width;
+
+	struct regulator *vdd;
+	struct regulator *avdd;
+
+	bool keys_en;
+
+	int (*power)(void *data, bool on);
+};
+
+#endif /* __LINUX_FTS_I2C_H */
